0f1662fa233ef9b9d5a60c67fb8203db212ff48c
Jussi Lindfors <jussi.lindfors@david.fi>
2017-06-12 10:44:12 +0300

    Correct api port handling

--- forge.sh
+++ forge.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+apiServerPort_var="${SERVER_PROPERTY_PREFIX}apiServerPort"
+
 if [ -z "${1}" ]; then
   echo "First arg needs to be secret phrase"
   exit 1
 fi
 
-url="http://localhost:${heat__apiServerPort:-7886}/nxt?requestType=startForging&secretPhrase=${1// /%20}"
+url="http://localhost:${!apiServerPort_var:-7886}/nxt?requestType=startForging&secretPhrase=${1// /%20}"
 if curl -X POST "${url}" -s | grep -q deadline ; then
   echo "Forging started"
   exit 0
